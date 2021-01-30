96a3ab5da336da4f5bb27f53a2b727467d3247b3
Gledi Caushaj <gledi.alb@gmail.com>
2021-01-30 14:48:01 +0100

    Initial commit

---
+++ movies/config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB = {
+    "host": os.getenv("DATABASE_HOST"),
+    "port": int(os.getenv("DATABASE_PORT", "3306")),
+    "user": os.getenv("DATABASE_USER"),
+    "password": os.getenv("DATABASE_PASSWORD"),
+    "database": os.getenv("DATABASE_NAME"),
+    "charset": os.getenv("DATABASE_CHARSET"),
+}
+
+DB_URI = os.getenv("DATABASE_URI")
